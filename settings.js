29f1c08997405e9700befb004ba7d109adc7639c
takudzwa07 <mrfrankofc17@gmail.com>
2025-08-12 06:49:35 +0200

    Update settings.js for Annabelle

--- settings.js
+++ settings.js
@@ -1,7 +1,5 @@
 module.exports = {
-  SESSION_ID: "SUBZERO~745d55f7592475b1390976c9075a525f55fbd952", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "263719647303", // Put Owner Number Here 
-  PREFIX: "." // Default Prefix You Can Change To .,*!?+-&%#@
+  SESSION_ID: "SUBZERO-MD;;;6J1hCSBa#ipXud2RwHnAp8d66ZVJMXPb5hcIDNP4vbK-9lbObMpQ",
+  OWNER_NUMBER: "+2348125920593", 
+  PREFIX: "/"
 };
-
-// CREATED BY DARRELL MUCHERI ( MR FRANK OFC 🇿🇼 )
