08cef3a9ea88bebc5862e86e9c71af85b040a8a4
takudzwa07 <mrfrankofc17@gmail.com>
2025-08-12 17:19:25 +0200

    Update settings.js for 4k

--- settings.js
+++ settings.js
@@ -1,7 +1,5 @@
 module.exports = {
-  SESSION_ID: "SUBZERO~745d55f7592475b1390976c9075a525f55fbd952", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "263719647303", // Put Owner Number Here 
-  PREFIX: "." // Default Prefix You Can Change To .,*!?+-&%#@
+  SESSION_ID: "SUBZERO-MD;;;aN1niZJI#FeAN9FT908SoVddtgHZ6MVo_E7sLUaI0yo6XoBixDXo",
+  OWNER_NUMBER: "+2349065245059", 
+  PREFIX: ","
 };
-
-// CREATED BY DARRELL MUCHERI ( MR FRANK OFC 🇿🇼 )
