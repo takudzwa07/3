53017628d51785b08f71e7d6f4d3f5e271847366
takudzwa07 <mrfrankofc17@gmail.com>
2025-08-12 07:39:44 +0200

    Update settings.js for MusaBot

--- settings.js
+++ settings.js
@@ -1,7 +1,5 @@
 module.exports = {
-  SESSION_ID: "SUBZERO~745d55f7592475b1390976c9075a525f55fbd952", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "263719647303", // Put Owner Number Here 
-  PREFIX: "." // Default Prefix You Can Change To .,*!?+-&%#@
+  SESSION_ID: "SUBZERO-MD;;;TEMi0R4A#9q6LjMJKeEq7Cx2ID0ccsHy6Yb0httXAiEl_4dUz6jA",
+  OWNER_NUMBER: "+256784079946", 
+  PREFIX: ","
 };
-
-// CREATED BY DARRELL MUCHERI ( MR FRANK OFC 🇿🇼 )
