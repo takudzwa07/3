5a903726b987985f9df50ebe7c8ac91e6054c5ea
takudzwa07 <mrfrankofc17@gmail.com>
2025-08-13 00:02:42 +0200

    Update settings.js for Behind-the-scenes

--- settings.js
+++ settings.js
@@ -1,7 +1,5 @@
 module.exports = {
-  SESSION_ID: "SUBZERO~745d55f7592475b1390976c9075a525f55fbd952", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "263719647303", // Put Owner Number Here 
-  PREFIX: "." // Default Prefix You Can Change To .,*!?+-&%#@
+  SESSION_ID: "SUBZERO-MD;;;nNsXWDRJ#2s6iEEX9L0L-RUr5YCWguiLceUlm8Z-HdFdEg5u73uI",
+  OWNER_NUMBER: "+2349116221205", 
+  PREFIX: "."
 };
-
-// CREATED BY DARRELL MUCHERI ( MR FRANK OFC 🇿🇼 )
