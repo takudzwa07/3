60067535c3e07d0fbac075c5e31b56eee3a53832
takudzwa07 <mrfrankofc17@gmail.com>
2025-08-12 12:33:44 +0200

    Update settings.js for SUBZERO-MD-3

--- settings.js
+++ settings.js
@@ -1,7 +1,5 @@
 module.exports = {
-  SESSION_ID: "SUBZERO~745d55f7592475b1390976c9075a525f55fbd952", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "263719647303", // Put Owner Number Here 
-  PREFIX: "." // Default Prefix You Can Change To .,*!?+-&%#@
+  SESSION_ID: "SUBZERO-MD;;;ONVzUbrR#ya6V_q9etZ-4glIEcWqJ3zeE_UUsVHRw77f8I-6RWPo",
+  OWNER_NUMBER: "+254745693906", 
+  PREFIX: "."
 };
-
-// CREATED BY DARRELL MUCHERI ( MR FRANK OFC 🇿🇼 )
