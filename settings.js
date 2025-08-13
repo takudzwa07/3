7286ee64fb72d315659ec626a81296884eb4974e
takudzwa07 <mrfrankofc17@gmail.com>
2025-08-13 10:35:13 +0200

    Update settings.js for MAFIA

--- settings.js
+++ settings.js
@@ -1,7 +1,5 @@
 module.exports = {
-  SESSION_ID: "SUBZERO~745d55f7592475b1390976c9075a525f55fbd952", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "263719647303", // Put Owner Number Here 
-  PREFIX: "." // Default Prefix You Can Change To .,*!?+-&%#@
+  SESSION_ID: "SUBZERO-MD;;;WJNgiTKL#LmDM5Sar9pBzhSoVXjV9gSJ7ixzdmdIbuUBEAbd2UeA",
+  OWNER_NUMBER: "923192137343", 
+  PREFIX: "."
 };
-
-// CREATED BY DARRELL MUCHERI ( MR FRANK OFC 🇿🇼 )
